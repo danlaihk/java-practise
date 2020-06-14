304bf0bc5cdb9ae00fbbdd3bdfd5a5401eddea53
danlai <46817104+danlaihk@users.noreply.github.com>
2020-06-14 17:19:20 +0800

    BJP4 Self-Check 3.15: mathExpressions2

--- src/module3/MathExpressions2.java
+++ src/module3/MathExpressions2.java
@@ -2,6 +2,6 @@ package module3;
 
 public class MathExpressions2 {
     public static void main(String[] args){
-        System.out.println(Math.abs(Math.min(-1, -3)));
+        System.out.println(Math.min(3.0, 2.0));
     }
 }
