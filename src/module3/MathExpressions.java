f7c81f0c702d82128a1c1a896f18300b3a67050d
danlai <46817104+danlaihk@users.noreply.github.com>
2020-06-14 16:27:16 +0800

    BJP4 Self-Check 3.13: mathExpressions

---
+++ src/module3/MathExpressions.java
@@ -0,0 +1,25 @@
+package module3;
+
+public class MathExpressions {
+    public static void main(String[] args){
+        System.out.println(Math.abs(-1.6)	);
+
+        System.out.println(Math.abs(2 + -4)	);
+
+        System.out.println(Math.pow(6, 2));
+
+        System.out.println(Math.pow(5 / 2, 6));
+
+        System.out.println(Math.ceil(9.1));
+
+        System.out.println(Math.max(7, 4));
+
+        System.out.println(Math.sqrt(76 + 45));
+
+        System.out.println(100 + Math.log10(100));
+
+        System.out.println(7 - 2 + Math.log10(1000) + Math.log(Math.pow(Math.E, 5))	);
+
+        System.out.println(Math.max(18 - 5 + Math.random(), Math.ceil(4.6 * 3))	);
+    }
+}
